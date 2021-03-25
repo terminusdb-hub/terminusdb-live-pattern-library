20d66cb22af019309cb21bc303a6d4d1009f3954
Kitty Jose <54106287+KittyJose@users.noreply.github.com>
2021-03-25 15:33:13 +0000

    adding react table

---
+++ packages/tdb-react-table/demo/makeData.js
@@ -0,0 +1,42 @@
+export const makeData = [
+  {
+    "Commit_num": 9,
+    "TimeStamp": "2021-03-16T00:00:00.000Z",
+    "UserName": "Cheukting"
+  },
+  {
+    "Commit_num": 13,
+    "TimeStamp": "2021-03-17T00:00:00.000Z",
+    "UserName": "Cheukting"
+  },
+  {
+    "Commit_num": 2,
+    "TimeStamp": "2021-03-17T00:00:00.000Z",
+    "UserName": "GavinMendelGleason"
+  },
+  {
+    "Commit_num": 3,
+    "TimeStamp": "2021-03-18T00:00:00.000Z",
+    "UserName": "Cheukting"
+  },
+  {
+    "Commit_num": 16,
+    "TimeStamp": "2021-03-23T00:00:00.000Z",
+    "UserName": "Cheukting"
+  },
+  {
+    "Commit_num": 2,
+    "TimeStamp": "2021-03-23T00:00:00.000Z",
+    "UserName": "github-actions[bot]"
+  },
+  {
+    "Commit_num": 11,
+    "TimeStamp": "2021-03-24T00:00:00.000Z",
+    "UserName": "Cheukting"
+  },
+  {
+    "Commit_num": 3,
+    "TimeStamp": "2021-03-25T00:00:00.000Z",
+    "UserName": "Cheukting"
+  }
+]
---
+++ packages/tdb-react-table/src/TDBReactTable.js
@@ -0,0 +1,17 @@
+import React from "react"
+import {useWorker} from "@terminusdb-live/react-worker"
+import {TableComponent} from '@terminusdb/terminusdb-react-table';
+
+export const TDBReactTable = (props) =>{
+    const startData= props.startData || []
+    const config=props.config || {}
+
+    const {onChange,error,loading,dataProvider} = useWorker(startData,props.onLoad)
+
+
+//only for test the style in external file
+    if(dataProvider.length>0){
+        return  <TableComponent columns={config.columns} data={dataProvider} />
+    }
+    return <div>LOADING</div>
+ }
